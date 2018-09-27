e92738e742b32d7aa192ee20ad05f740e46f5f7a
Julian Peters <julianpeters@tiot.de>
2018-09-27 18:31:11 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -33,6 +33,10 @@ function switchAge(age) {
     case 13:
     case 14:
     case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
       return "You are a teenager"
     default:
       return "You have an age"
