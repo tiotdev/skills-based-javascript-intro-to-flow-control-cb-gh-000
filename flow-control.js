8dba9795c2fde5b7f26e46813bed41b1b574510e
Julian Peters <julianpeters@tiot.de>
2018-09-27 18:31:26 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -13,16 +13,17 @@ function teenager(age) {
   }
 }
 
-// function ageChecker(age) {
-//   if(age >= 13 && age <= 19) {
-//     return "You are a teenager!"
-//   }
-//   else if(age <= 12){
-//     return "You are a kid"
-//   }
-//   else if(age > 19) {
-//     return "You are a grownup"
-//   }
+function ageChecker(age) {
+  if(age >= 13 && age <= 19) {
+    return "You are a teenager!"
+  }
+  else if(age <= 12){
+    return "You are a kid"
+  }
+  else if(age > 19) {
+    return "You are a grownup"
+  }
+}
 
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
