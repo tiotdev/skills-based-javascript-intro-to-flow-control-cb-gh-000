c1665018d03078dde65019d25e99dedb1b010c8c
Julian Peters <julianpeters@tiot.de>
2018-09-27 18:26:19 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age){
-    case 13-19:
+    case 15:
       return "You are a teenager!"
     default:
       return "You have an age"
